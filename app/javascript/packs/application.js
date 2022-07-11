8fa2ec46860e3b72541a50fc59972f75c30f8d1e
K-M0613 <yishumenma245@gmail.com>
2022-07-11 01:21:18 +0000

    9b

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import jQuery from "jquery"
@@ -14,9 +13,10 @@ import "bootstrap"
 
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application" 
+require("chartkick")
+require("chart.js")
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
 
 global.$ = jQuery;
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,14 +4,24 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_tags, dependent: :destroy
   has_many :tags, through: :book_tags
-  
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_2days, -> { where(created_at: 2.days.ago.all_day) }
+  scope :created_3days, -> { where(created_at: 3.days.ago.all_day) }
+  scope :created_4days, -> { where(created_at: 4.days.ago.all_day) }
+  scope :created_5days, -> { where(created_at: 5.days.ago.all_day) }
+  scope :created_6days, -> { where(created_at: 6.days.ago.all_day) }
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  
+
   def save_tags(savebook_tags)
     # 現在のユーザーの持っているskillを引っ張ってきている
     current_tags = self.tags.pluck(:name) unless self.tags.nil?
@@ -19,12 +29,12 @@ class Book < ApplicationRecord
     old_tags = current_tags - savebook_tags
     # 今回保存されたものと現在の差を新しいタグとする。新しいタグは保存
     new_tags = savebook_tags - current_tags
-		
+
     # Destroy old taggings:
     old_tags.each do |old_name|
       self.tags.delete Tag.find_by(name:old_name)
     end
-		
+
     # Create new taggings:
     new_tags.each do |new_name|
       book_tag = Tag.find_or_create_by(name:new_name)
@@ -32,7 +42,7 @@ class Book < ApplicationRecord
       self.tags << book_tag
     end
   end
-	
+
   def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
